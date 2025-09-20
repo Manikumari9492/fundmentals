e23d01bbc3ed2969c4be0ed6e9d57efd9836df55
Manikumari9492 <manikumari8639@gmail.com>
2025-09-20 19:06:23 +0530

    Add files via upload
    jfs41

---
+++ Human_main.java
@@ -0,0 +1,12 @@
+package com.codegnan.oop;
+
+public class Human_main {
+
+	public static void main(String[] args) {
+		Human human1=new Human();
+		human1.setName("mani");
+		human1.setAge(20);
+		System.out.println("Name :"+human1.getName()+"Age :"+human1.getAge());
+}
+
+}
