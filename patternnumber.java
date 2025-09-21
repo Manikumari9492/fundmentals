3b24f6728d0ef830c0fe01dad017322edf61a89a
Manikumari9492 <manikumari8639@gmail.com>
2025-09-21 18:20:42 +0530

    jfs41

---
+++ patternnumber.java
@@ -0,0 +1,18 @@
+package com.codegnan.mani;
+
+public class patternnumber {
+
+	public static void main(String[] args) {
+		int i;
+		int j;
+		int n=5;
+		for(i=1;i<=n;i++){
+			for(j=1;j<=n;j++) {
+				System.out.print(i+" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
