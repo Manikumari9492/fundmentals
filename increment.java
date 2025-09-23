7a8b4c795fc59e223057398d7a583f15b8a1d2ff
Manikumari9492 <manikumari8639@gmail.com>
2025-09-23 18:55:25 +0530

    jfs41

---
+++ increment.java
@@ -0,0 +1,11 @@
+package com.codegnan.mani;
+
+public class increment {
+
+	public static void main(String[] args) {
+		int m=10,n=10;
+		int res =m++/(++n*n--)/--m;
+		System.out.println(res);
+	}
+
+}
