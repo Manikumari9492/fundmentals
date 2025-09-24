c21b368dca0ad0f801291afb4bd98df03979b322
Manikumari9492 <manikumari8639@gmail.com>
2025-09-24 12:16:47 +0530

    jfs41

---
+++ snippet.java
@@ -0,0 +1,16 @@
+package com.codegnan.mani;
+
+public class snippet {
+
+	public static void main(String[] args) {
+		byte b;
+		int i=258;
+		double d =325.59;
+		 b=(byte)i;
+		 System.out.println(b);
+		 i=(int)d;
+		 System.out.println(i);
+
+	}
+
+}
