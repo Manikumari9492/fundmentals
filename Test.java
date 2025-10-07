a3c0d25adfcfecd5ab8c0ffa3c40fa6b299e884a
Manikumari9492 <manikumari8639@gmail.com>
2025-10-07 21:45:18 +0530

    jfs41

---
+++ Test.java
@@ -0,0 +1,9 @@
+package com.codegnan.Strings;
+
+public class Test {
+	public static void main(String[] args) {
+		StringBuilder s=new StringBuilder("Codegnan");
+		s.append("Solutions");
+		System.out.println(s);
+	}
+}
