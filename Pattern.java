e4423d0cad11999607b156a9de87280bda3b2437
Manikumari9492 <manikumari8639@gmail.com>
2025-10-11 15:45:31 +0530

    jfs41

---
+++ Pattern.java
@@ -0,0 +1,18 @@
+package com.codegnan.mani;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		int rows=5;
+		for(int i=1;i<=rows;i++) {
+			for(int j=i;j<=i+i-1;j++) {
+				System.out.print(j+" ");
+			}
+			for(int j=i+i-2;j>=i;j--) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
