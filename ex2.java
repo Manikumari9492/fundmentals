493b8637c8f2d8c497c67ebd8134d404053bfa32
Manikumari9492 <manikumari8639@gmail.com>
2025-10-13 21:41:56 +0530

    jfs41

---
+++ ex2.java
@@ -0,0 +1,16 @@
+package com.codegnan.mani;
+
+import java.util.Scanner;
+
+
+public class ex2 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		int c=sc.nextInt();
+		float f=(c*9.0f/5)+32;
+		System.out.println(f);
+	}
+
+}
