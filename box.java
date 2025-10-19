b79de5be7023216b72e4a8689477acbbd75cbb64
Manikumari9492 <manikumari8639@gmail.com>
2025-10-19 11:52:11 +0530

    jfs41

---
+++ box.java
@@ -0,0 +1,22 @@
+package com.codegnan.mani;
+
+public class box {
+
+	public static void main(String[] args) {
+		int i;
+		int j;
+		int n=5;
+		for(i=1;i<=n;i++) {
+			for(j=1;j<=n;j++) {
+				if(i==1||i==n||
+						j==1||j==n) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+		System.out.println();
+		}
+	}
+
+}
