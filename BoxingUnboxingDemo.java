65982576ea294bc5085ac72d579c2661a885f88a
Manikumari9492 <manikumari8639@gmail.com>
2025-10-21 17:33:10 +0530

    jfs41

---
+++ BoxingUnboxingDemo.java
@@ -0,0 +1,16 @@
+package com.codegnan.collections;
+
+public class BoxingUnboxingDemo {
+
+	public static void main(String[] args) {
+		int a=5;
+		Integer obj=a;
+		System.out.println("Primitive :"+a);
+		System.out.println("Object(Wrapper):"+obj);
+		Double obj1=10.5;//object
+		double d=obj1;//object to primitive unboxing
+		System.out.println("Wrapper Object:"+obj1);
+		System.out.println("Primitive value:"+d);
+	}
+
+}
