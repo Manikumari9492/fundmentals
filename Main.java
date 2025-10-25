8aa9665acf0c192fe204c662336a2b383aed6426
Manikumari9492 <manikumari8639@gmail.com>
2025-10-25 17:57:47 +0530

    jfs41

---
+++ Main.java
@@ -0,0 +1,19 @@
+package com.codegnan.Map;
+
+import java.util.TreeMap;
+
+public class Main {
+
+	public static void main(String[] args) {
+	TreeMap<String,String>t=new TreeMap<>();
+	t.put("chinnu", "chinna");
+	t.put("bunny", "sunny");
+	t.put("gunnu", "ganni");
+	System.out.println(t);
+	//t.put("aaa", 10);
+	//System.out.println(t);
+	
+	
+	}
+
+}
