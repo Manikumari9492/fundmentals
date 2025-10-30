e36f489f09229207a6b4318a74b02fa283886a2a
Manikumari9492 <manikumari8639@gmail.com>
2025-10-30 15:36:03 +0530

    jfs41

---
+++ FilterExample.java
@@ -0,0 +1,24 @@
+package com.codegnan.utilStreams;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class FilterExample {
+
+	public static void main(String[] args) {
+		List<Integer>numbers=new ArrayList<>();
+		for(int i=0;i<=10;i++) {
+			numbers.add(i);
+		}
+		System.out.println(numbers);
+		Stream<Integer>s=numbers.stream();
+		Stream<Integer>evenNumbers=s.filter(i->i%2==0);
+		//System.out.println(evenNumbers);
+		evenNumbers.forEach(System.out::println);
+		System.out.println("map");
+		numbers.stream().map(i->i+2).forEach(System.out::println);
+		
+	}
+
+}
