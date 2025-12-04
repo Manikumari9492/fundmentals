b28b0dc4b845b735bcf83ab37df9a25d947cbe87
Manikumari9492 <manikumari8639@gmail.com>
2025-12-04 18:30:58 +0530

    jfs41

---
+++ Example1.java
@@ -0,0 +1,23 @@
+package com.codegnan.mani;
+import java.util.Scanner;
+public class Example1 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		//int b=sc.nextInt();
+		//int c=sc.nextInt();
+		
+		
+		int square=a*a;
+		//int sum2=a+c;
+		//int sum3=b+c;
+		
+		
+		System.out.println(square);
+		//System.out.println(sum2);
+		//System.out.println(sum3);
+		
+	}
+
+}
