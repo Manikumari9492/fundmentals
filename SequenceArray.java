35a188f8485e9d73d7c0900a56902afc8c03ae46
Manikumari9492 <manikumari8639@gmail.com>
2026-02-02 11:48:41 +0530

    Add files via upload

---
+++ SequenceArray.java
@@ -0,0 +1,18 @@
+package Core___Java;
+
+public class SequenceArray {
+
+	public static void main(String[] args) {
+	 int n=10;
+	 int arr[]=new int[n];
+	 for(int i=0;i<n;i++) {
+		 arr[i]=i*+1;
+		 
+	 }
+	 for(int num:arr) {
+		 System.out.print(num+" ");
+		 
+	 }
+	}
+
+}
